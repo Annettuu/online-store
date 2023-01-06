cc942134fe9527077bf4b6f7abd7f0de5707b817
Annettuu <122024704+Annettuu@users.noreply.github.com>
2023-01-07 01:25:30 +0700

    Подготовила базу для разработки (#7)
    Co-authored-by: Anna Bolotova <a.s.bolotova@lordmihei.ru>

---
+++ frontend/online-store/src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+   работает!
+   <HelloWorld/>
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
