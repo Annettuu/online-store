f82c70e77e90026b6e8eb850cdf6a0f370e8b786
Annettuu <122024704+Annettuu@users.noreply.github.com>
2023-01-09 02:46:03 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <Header/>
+    <search-bar/>
+    <product-list/>
+  </div>
+
+</template>
+
+<script>
+import Header from '@/components/Home/Header.vue'
+import SearchBar from "@/components/Home/searchBar";
+import ProductList from "@/components/Home/productList";
+
+export default {
+  name: 'HomeView',
+  components: {
+    ProductList,
+    SearchBar,
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import  'bootstrap/dist/css/bootstrap.css';
+@import  'bootstrap-vue/dist/bootstrap-vue.css';
+@import  '/scss/custom.css';
+
+.home {
+  background: #173f35;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+</style>
