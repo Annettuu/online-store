cb2f9a12db292787c08e570e1521893960001131
Annettuu <122024704+Annettuu@users.noreply.github.com>
2023-01-09 02:31:28 +0700

    Сделала разметку для главной страницы (#10)
    Co-authored-by: Anna Bolotova <a.s.bolotova@lordmihei.ru>

---
+++ frontend/online-store/src/components/Home/Header.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="hello">
+   <nav class="navbar">
+     <a class="navbar-brand" href="#">Логотип магазина</a>
+       <b-navbar-nav class="buttons">
+         <b-button>
+           Корзина
+         </b-button>
+         <b-button>
+           Продавцу
+         </b-button>
+       </b-navbar-nav>
+   </nav>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.hello {
+  width: 100%;
+}
+.buttons {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+}
+
+</style>
---
+++ frontend/online-store/src/components/Home/searchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container searchBar">
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Поиск">
+      <b-button>Поиск</b-button>
+      <b-button>Фильтр</b-button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "searchBar"
+}
+</script>
+
+<style lang="scss">
+.searchBar {
+  margin-top: 50px;
+}
+.d-flex {
+  gap: 5px;
+}
+</style>
